e25425653d15770bede0cc81868dd5673e74f4da
marguerite dorangeon <margueritedorangeon@hotmail.fr>
2017-12-05 11:56:08 +0100

    adding user_id et item_id to selections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205095810) do
+ActiveRecord::Schema.define(version: 20171205105414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20171205095810) do
   create_table "selections", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_selections_on_item_id"
+    t.index ["user_id"], name: "index_selections_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,4 +76,6 @@ ActiveRecord::Schema.define(version: 20171205095810) do
   end
 
   add_foreign_key "items", "brands"
+  add_foreign_key "selections", "items"
+  add_foreign_key "selections", "users"
 end
