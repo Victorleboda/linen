87c18fef3b406152695af16ce19d924d2a696c6b
marguerite dorangeon <margueritedorangeon@hotmail.fr>
2017-12-05 17:21:28 +0100

    adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ cachemire = Material.create(name: "cachemire")
 # , climate_impact: , water_impact: , air_impact: )
 chanvre = Material.create(name: "chanvre")
 # , climate_impact: , water_impact: , air_impact: )
-coton = Material.create(name: "coton", climate_impact: 10, water_impact: 40, air_impact: 30)
+coton = Material.create(name: "coton", climate_impact: 10, water_impact: 10, air_impact: 10)
 jute = Material.create(name: "jute")
 # , climate_impact: , water_impact: , air_impact: )
 laine_de_chameau = Material.create(name: "laine de chameau")
@@ -60,12 +60,25 @@ item1 = Item.new(url: url1, photo: photo1, price: 23.99, category: "pull", produ
 item1.brand = Brand.new(name: "New Look")
 item1.save!
 
+url2 = "http://www.asos.fr/new-look/new-look-pull-color-block-noir-et-gris/prd/9080710?clr=motifnoir&SearchQuery=&cid=7617&gridcolumn=4&gridrow=7&gridsize=4&pge=1&pgesize=72&totalstyles=1172"
+photo2 = "http://images.asos-media.com/products/new-look-pull-color-block-noir-et-gris/9080710-1-blackpattern?$XXL$&wid=513&fit=constrain"
+item2 = Item.new(url: url1, photo: photo1, price: 23.99, category: "pull", product_code: "1210863")
+item2.brand = Brand.new(name: "New Look")
+item2.save!
+
 puts "creating assemblings"
 assembling1 = Assembling.new(percent: 100)
 assembling1.material = coton
 assembling1.item = item1
 assembling1.save!
 
+assembling2 = Assembling.new(percent: 100)
+assembling2.material = coton
+assembling2.item = item2
+assembling2.save!
+
 puts "creating selections"
 selection1 = Selection.new(user: seb, item: item1)
 selection1.save!
+selection2 = Selection.new(user: seb, item: item2)
+selection2.save!
