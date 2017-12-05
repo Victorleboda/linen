231202b7e089337da9bc5a838892cc10c4456cfb
Donatien Rolland <donatien@rollandmail.com>
2017-12-05 17:24:03 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 puts "destroy all materials, items, assemblings, selections and users"
-
 Material.destroy_all
 Item.destroy_all
 Assembling.destroy_all
@@ -20,7 +19,7 @@ cachemire = Material.create(name: "cachemire")
 # , climate_impact: , water_impact: , air_impact: )
 chanvre = Material.create(name: "chanvre")
 # , climate_impact: , water_impact: , air_impact: )
-coton = Material.create(name: "coton", climate_impact: 10, water_impact: 40, air_impact: 30)
+coton = Material.create(name: "coton", climate_impact: 10, water_impact: 10, air_impact: 10)
 jute = Material.create(name: "jute")
 # , climate_impact: , water_impact: , air_impact: )
 laine_de_chameau = Material.create(name: "laine de chameau")
@@ -61,58 +60,25 @@ item1 = Item.new(url: url1, photo: photo1, price: 23.99, category: "pull", produ
 item1.brand = Brand.new(name: "New Look")
 item1.save!
 
+url2 = "http://www.asos.fr/new-look/new-look-pull-color-block-noir-et-gris/prd/9080710?clr=motifnoir&SearchQuery=&cid=7617&gridcolumn=4&gridrow=7&gridsize=4&pge=1&pgesize=72&totalstyles=1172"
+photo2 = "http://images.asos-media.com/products/new-look-pull-color-block-noir-et-gris/9080710-1-blackpattern?$XXL$&wid=513&fit=constrain"
+item2 = Item.new(url: url1, photo: photo1, price: 23.99, category: "pull", product_code: "1210863")
+item2.brand = Brand.new(name: "New Look")
+item2.save!
+
 puts "creating assemblings"
 assembling1 = Assembling.new(percent: 100)
 assembling1.material = coton
 assembling1.item = item1
 assembling1.save!
-=======
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-angora = Material.new(name: "Fil d'angora")
-# , climate_impact: -3.5e+1, water_impact: 2.2e-3, air_impact:)
-cachemire = Material.new(name: "Fil de cachemire")
-# , climate_impact: , water_impact: , air_impact:)
-chanvre = Material.new(name: "Fil de chanvre")
-# , climate_impact: , water_impact: , air_impact:)
-coton = Material.new(name: "Fil de coton")
-# , climate_impact: , water_impact: , air_impact:)
-jute = Material.new(name: "Fil de jute")
-# , climate_impact: , water_impact: , air_impact:)
-laine_de_chameau = Material.new(name: "Fil de laine de chameau")
-# , climate_impact: , water_impact: , air_impact:)
-laine_de_mouton = Material.new(name: "Fil de laine de mouton")
-# , climate_impact: , water_impact: , air_impact:)
-lin = Material.new(name: "Fil de lin (étoupe)")
-# , climate_impact: , water_impact: , air_impact:)
-lin2 = Material.new(name: "Fil de lin (filasse)")
-# , climate_impact: , water_impact: , air_impact:)
-soie = Material.new(name: "Fil de soie")
-# , climate_impact: , water_impact: , air_impact:
-polypropylene = Material.new(name: "Filament bi-composant polypropylène/polyamide")
-# , climate_impact: , water_impact: , air_impact:)
-acrylique = Material.new(name: "Filament d'acrylique")
-# , climate_impact: , water_impact: , air_impact:)
-aramide = Material.new(name: "Filament d'aramide")
-# , climate_impact: , water_impact: , air_impact:)
-polyamide = Material.new(name: "Filament de polyamide 66")
-# , climate_impact: , water_impact: , air_impact:)
-pokyester = Material.new(name: "Filament de polyester")
-# , climate_impact: , water_impact: , air_impact:)
-polyethykene = Material.new(name: "Filament de polyéthylène")
-# , climate_impact: , water_impact: , air_impact:)
-polylactide = Material.new(name: "Filament de polylactide")
-# , climate_impact: , water_impact: , air_impact:)
-polypropylene = Material.new(name: "Filament de polypropylène")
-# , climate_impact: , water_impact: , air_impact:)
-polyuréthane = Material.new(name: "Filament de polyuréthane")
-# , climate_impact: , water_impact: , air_impact:)
-viscose = Material.new(name: "Filament de viscose")
-# , climate_impact: , water_impact: , air_impact:)
 
+assembling2 = Assembling.new(percent: 100)
+assembling2.material = coton
+assembling2.item = item2
+assembling2.save!
 
 puts "creating selections"
 selection1 = Selection.new(user: seb, item: item1)
 selection1.save!
+selection2 = Selection.new(user: seb, item: item2)
+selection2.save!
