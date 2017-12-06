79c4fe90fe16f5262a7f1024d2f519e3e01a3168
victor ledoda <lebodavictor@gmail.com>
2017-12-06 09:07:26 +0100

    scraper file for linen plugin

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+
+  def new
+    @item = Item.new
+  end
+# si c'est une page produit, faire une requete http au server (mettre scraper dans private)
+# enregistrer dans database
+  def create
+    @item = Item.new(items_params)
+    # if @item.save
+    # else
+    #   render :new
+    # end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+
+  private
+
+  def items_params
+    params.require(:items).permit(:category, :brand_id, :url, :photo, :price, :product_code)
+  end
 end
