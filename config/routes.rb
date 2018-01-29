4090d5c85676013d562676b58abc76331ac8dcf2
marguerite dorangeon <margueritedorangeon@hotmail.fr>
2018-01-29 10:09:49 +0100

    facebook connect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
   resources :items, only: [ :index, :show ]
